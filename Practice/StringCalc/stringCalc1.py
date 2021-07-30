79a9d027375668d6f37684fe3a9aabb9503d4912
ingyeomental <83263943+thanks-kim@users.noreply.github.com>
2021-07-30 10:47:54 +0900

    Create stringCalc1.py

---
+++ Practice/StringCalc/stringCalc1.py
@@ -0,0 +1,22 @@
+import re
+
+def str_calc(str_val: str) -> int: 
+    p = re.compile('[\*,\+,\-,\/]')
+    notation = p.search(str_val).group()
+    num_lst = [int(i) for i in str_val.split(notation)]
+    
+    if notation == '+':
+        return num_lst[0] + num_lst[1]
+    elif notation == '-':
+        return num_lst[0] - num_lst[1]
+    elif notation == '*':
+        return num_lst[0] * num_lst[1]
+    elif notation == '/':
+        try:
+            return num_lst[0] / num_lst[1]
+        except ZeroDivisionError as z:
+            return z
+
+
+if __name__ == '__main__':
+    print(str_calc('1/0'))
