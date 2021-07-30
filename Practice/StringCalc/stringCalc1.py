7995c503692bafe964412f95686690b4709175a7
ingyeomental <46236396+ingyeomental@users.noreply.github.com>
2021-07-30 14:53:05 +0900

    데이터 타입 수정
    나눗셈 할 때 실수형이 나올 수 있기 때문

--- Practice/StringCalc/stringCalc1.py
+++ Practice/StringCalc/stringCalc1.py
@@ -1,9 +1,9 @@
 import re
 
-def str_calc(str_val: str) -> int: 
+def str_calc(str_val: str) -> float: 
     p = re.compile('[\*,\+,\-,\/]')
     notation = p.search(str_val).group()
-    num_lst = [int(i) for i in str_val.split(notation)]
+    num_lst = [float(i) for i in str_val.split(notation)]
     
     if notation == '+':
         return num_lst[0] + num_lst[1]
@@ -19,4 +19,4 @@ def str_calc(str_val: str) -> int:
 
 
 if __name__ == '__main__':
-    print(str_calc('1/0'))
+    print(str_calc('1/5'))
