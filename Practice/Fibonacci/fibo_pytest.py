171bbba5d34da41bef13a0b0b7d2c5cb219d57da
ingyeomental <46236396+ingyeomental@users.noreply.github.com>
2021-08-06 15:57:38 +0900

    fibonacci tdd-2
    세번째 항 이상부터 테스트

--- Practice/Fibonacci/fibo_pytest.py
+++ Practice/Fibonacci/fibo_pytest.py
@@ -5,3 +5,12 @@ def test_fibo_0():
 
 def test_fibo_1():
     assert fibo.fibonacci(1) == 1
+
+def test_fibo_2():
+    assert fibo.fibonacci(2) == 2
+
+def test_fibo_3():
+    assert fibo.fibonacci(3) == 3
+
+def test_fibo_3():
+    assert fibo.fibonacci(4) == 5
